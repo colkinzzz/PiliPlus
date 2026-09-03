9d153544d55fec93af900a39d9c03c1e1b0c76ec
colkinzzz <139250198+colkinzzz@users.noreply.github.com>
2026-09-03 14:16:58 +0800

    fix(car): import vehicle preferences in video view

--- lib/pages/video/view.dart
+++ lib/pages/video/view.dart
@@ -67,6 +67,7 @@ import 'package:PiliPlus/utils/page_utils.dart';
 import 'package:PiliPlus/utils/platform_utils.dart';
 import 'package:PiliPlus/utils/storage.dart';
 import 'package:PiliPlus/utils/storage_key.dart';
+import 'package:PiliPlus/utils/storage_pref.dart';
 import 'package:PiliPlus/utils/theme_utils.dart';
 import 'package:extended_nested_scroll_view/extended_nested_scroll_view.dart';
 import 'package:flutter/foundation.dart' show kDebugMode, clampDouble;
@@ -2053,3 +2054,4 @@ class _VideoDetailPageVState extends State<VideoDetailPageV>
     );
   }
 }
+
